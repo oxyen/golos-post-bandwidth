aeb300d5576af5308642dbb9da406ea4bcb38b90
oxyen <mepesov@yahoo.com>
2017-02-17 12:18:36 -0800

    initial

---
+++ comments-vs-votes.R
@@ -0,0 +1,13 @@
+library(RODBC)
+library(dplyr)
+dbhandle <- odbcDriverConnect('driver={SQL Server};server=sql.golos.cloud;UID=golos;PWD=golos;CharSet=utf8')
+#sqlQuery(dbhandle, 'select * from information_schema.tables')
+
+# Posts in February with at least one vote
+p.w.c <- sqlQuery(dbhandle, 'SELECT Created, url, total_vote_weight, category, net_votes, children, total_payout_value, total_pending_payout_value, author FROM Comments WHERE depth=0 AND Created > \'2017-2-1\' AND net_votes > 0 ORDER BY ID DESC')
+p.w.c$cpervote <- p.w.c$children/p.w.c$net_votes
+p.w.c$cpervote %>% summary
+sorted <- p.w.c[order(-p.w.c$cpervote), ]
+sorted$url[1:10]
+
+odbcClose(dbhandle)
