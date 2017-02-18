0a8bf59adee77fb5e87a2c32f05ef71feb133ba6
oxyen <mepesov@yahoo.com>
2017-02-17 18:49:36 -0800

    initial

---
+++ most-new-followers-on-new.R
@@ -0,0 +1,4 @@
+﻿library(RODBC)
+dbhandle <- odbcDriverConnect('driver={SQL Server};server=sql.golos.cloud;UID=golos;PWD=golos')
+sqlQuery(dbhandle, 'select top 20 following, count(*) from txCustomsFollows where what=\'["blog"]\' and timestamp BETWEEN \'2017-02-10\' AND \'2017-02-17\' AND following not in (select top 100 following from txCustomsFollows where what=\'["blog"]\' and timestamp < \'2017-02-10\' group by following order by count(*) desc) group by following order by count(*) desc')
+odbcClose(dbhandle)
